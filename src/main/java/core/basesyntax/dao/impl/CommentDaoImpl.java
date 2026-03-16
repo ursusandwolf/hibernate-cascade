d0f8724a8881a917a383329c62b542a51dc51801
alex <ursusandwolf@protonmail.com>
2026-03-16 15:50:57 +0200

    remove comment method

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -3,7 +3,9 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.CommentDao;
 import core.basesyntax.model.Comment;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class CommentDaoImpl extends AbstractDao implements CommentDao {
     public CommentDaoImpl(SessionFactory sessionFactory) {
@@ -27,6 +29,22 @@ public class CommentDaoImpl extends AbstractDao implements CommentDao {
 
     @Override
     public void remove(Comment entity) {
-
+        Session currentSession = null;
+        Transaction transaction = null;
+        try {
+            currentSession = factory.openSession();
+            transaction = currentSession.beginTransaction();
+            currentSession.remove(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't remove entity " + entity, e);
+        } finally {
+            if (currentSession != null) {
+                currentSession.close();
+            }
+        }
     }
 }
