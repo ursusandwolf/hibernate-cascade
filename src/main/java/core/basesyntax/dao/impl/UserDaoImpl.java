77ff3aada39e7472b7f4b79f13f15f346f446d51
alex <ursusandwolf@protonmail.com>
2026-03-17 16:48:12 +0200

    remove User but retain comments

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -1,11 +1,20 @@
 package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.UserDao;
+import core.basesyntax.model.Comment;
 import core.basesyntax.model.User;
 import org.hibernate.SessionFactory;
 
 public class UserDaoImpl extends AbstractDao<User> implements UserDao {
     public UserDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory, User.class);
+    }
+
+    @Override
+    public void remove(User user) {
+        for (Comment c : user.getComments()) {
+            c.setUser(null);
+        }
+        super.remove(user);
     }
 }
